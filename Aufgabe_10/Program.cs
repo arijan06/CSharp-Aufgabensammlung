390f6d56cec460044fbb7338918fec759f58e23f
arijan <arijan.krasniqi-9@outlook>
2025-09-30 16:43:25 +0200

    Aufgabe 10 gemacht

--- Aufgabe_10/Program.cs
+++ Aufgabe_10/Program.cs
@@ -4,7 +4,25 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hallo Welt!");
+            Console.Write("Bitte gib eine positive ganze Zahl ein: ");
+            int zahl = Convert.ToInt32(Console.ReadLine());
+
+            int quersumme = BerechneQuersumme(zahl);
+
+            Console.WriteLine($"Die Quersumme von {zahl} ist: {quersumme}");
+        }
+
+        static int BerechneQuersumme(int zahl)
+        {
+            int sum = 0;
+
+            while (zahl != 0)
+            {
+                sum = sum + (zahl % 10);
+                zahl = zahl / 10;
+            }
+
+            return sum;
         }
     }
 }
